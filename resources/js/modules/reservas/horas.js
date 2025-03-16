00c82dc2be2e0fef49c5b68e82fdf40839f5bf86
Willzin1 <william.mendonca34@gmail.com>
2025-03-16 15:17:50 -0300

    Alterado verificacao input

--- resources/js/modules/reservas/horas.js
+++ resources/js/modules/reservas/horas.js
@@ -1,6 +1,8 @@
 export default function() {
     const horaInput = document.getElementById('hora');
 
+    if (!horaInput) return;
+
     for (let h = 9; h <= 20; h++) { // Hora de 9h até 20h30
         for (let m = 0; m <= 30; m += 30) {
             const timeOption = `${h}:${m.toString().padStart(2, '0')}`;
