45760093c551e2f29b36c6bb5d0a64cb439b8907
Willzin1 <william.mendonca34@gmail.com>
2025-05-30 14:41:07 -0300

    Feat: refactor PWA icon links by moving to separate include file

--- resources/views/templates/master.blade.php
+++ resources/views/templates/master.blade.php
@@ -13,7 +13,7 @@
     <title>@yield('title')</title>
 
     <link rel="manifest" href="{{ asset('manifest.json') }}">
-    @vite(['resources/includes/links.html'])
+    @include('includes.links')
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
