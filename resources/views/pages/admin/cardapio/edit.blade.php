a06e8d47cc7eaf30d5e28b049e03e9aef931422b
Willzin1 <william.mendonca34@gmail.com>
2025-06-19 21:53:54 -0300

    Style: enhance form layout and button grouping for better usability in cardapio management

--- resources/views/pages/admin/cardapio/edit.blade.php
+++ resources/views/pages/admin/cardapio/edit.blade.php
@@ -1,42 +1,46 @@
 @extends('templates.master')
 
-@section('title')
-Sustenta Food | Admin
-@endsection
+@section('title', 'Sustenta Food | Admin')
 
 @section('content')
 <div class="dashboard-container">
     @include('includes.aside')
     <div class="containerGerente">
-        <div class="container-reservas">
-            <h2>Editar Prato</h2>
+        <div class="dashboard-content">
 
-            <form action="{{ route('admin.cardapio.update', ['prato' => $prato['id']]) }}" class="prato-form" method="POST" enctype="multipart/form-data">
-                @csrf
-                @method('PUT')
+            <div class="dashboard-dishes">
 
-                <label for="nome" class="form-label">Nome do Prato</label>
-                <input type="text" class="form-control validar nome" id="nome" name="nome" value="{{ $prato['nome'] }}">
+                <h2>Editar Prato</h2>
 
-                <label for="descricao" class="form-label">Descrição</label>
-                <textarea class="form-control validar descricao" id="descricao" name="descricao" rows="3">{{ $prato['descricao'] }}</textarea>
+                <form action="{{ route('admin.cardapio.update', ['prato' => $prato['id']]) }}" class="prato-form" method="POST" enctype="multipart/form-data">
+                    @csrf
+                    @method('PUT')
 
-                <label for="categoria">Categoria</label>
-                <select name="categoria" id="categoria" class="validar categoria" required>
-                    <option value="{{ $prato['categoria'] }}">{{ $prato['categoria'] }}</option>
-                    <option value="Entradas">Entradas</option>
-                    <option value="Prato principal">Prato principal</option>
-                    <option value="Sobremesas">Sobremesas</option>
-                    <option value="Cardapio infantil">Cardápio infantil</option>
-                    <option value="Bebidas">Bebidas</option>
-                </select>
+                    <label for="nome" class="form-label">Nome do Prato</label>
+                    <input type="text" class="form-control validar nome" id="nome" name="nome" value="{{ $prato['nome'] }}">
 
-                <label for="imagem" class="form-label">Imagem</label>
-                <input type="file" class="form-control validar imagem" id="imagem" name="imagem">
+                    <label for="descricao" class="form-label">Descrição</label>
+                    <textarea class="form-control validar descricao" id="descricao" name="descricao" rows="3">{{ $prato['descricao'] }}</textarea>
 
-                <button type="submit" class="btn btn-primary">Salvar alterações</button>
-                <a href="{{ route('admin.cardapio.index') }}" class="btn-link btn-link-dark">Voltar</a>
-            </form>
+                    <label for="categoria">Categoria</label>
+                    <select name="categoria" id="categoria" class="validar categoria" required>
+                        <option value="{{ $prato['categoria'] }}">{{ $prato['categoria'] }}</option>
+                        <option value="Entradas">Entradas</option>
+                        <option value="Prato principal">Prato principal</option>
+                        <option value="Sobremesas">Sobremesas</option>
+                        <option value="Cardapio infantil">Cardápio infantil</option>
+                        <option value="Bebidas">Bebidas</option>
+                    </select>
+
+                    <label for="imagem" class="form-label">Imagem</label>
+                    <input type="file" class="form-control validar imagem" id="imagem" name="imagem">
+
+                    <div class="btn-group">
+                        <button type="submit">Salvar alterações</button>
+                        <a href="{{ route('admin.cardapio.index') }}" class="btnA">Voltar</a>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
 </div>
--- resources/views/pages/admin/cardapio/index.blade.php
+++ resources/views/pages/admin/cardapio/index.blade.php
@@ -1,66 +1,62 @@
 @extends('templates.master')
 
-@section('title')
-Sustenta Food | Admin
-@endsection
+@section('title', 'Sustenta Food | Admin')
 
 @section('content')
 <div class="dashboard-container">
-
     @include('includes.aside')
     <div class="containerGerente">
-        <div class="container-reservas">
+        <div class="dashboard-content">
             <section id="reservas">
 
-            <h2>Gerenciar Cardápio</h2>
-            <div class="reservas-tabela"> <!-- .Colocar um estilizi~]ao diferenciada -->
-            <a href="{{ route('admin.cardapio.create') }}" class="btn-link btn-link-dark">Adicionar Prato</a>
+                <h2>Gerenciar Cardápio</h2>
+                <div class="reservas-tabela"> <!-- Colocar um estilização diferenciada -->
 
-            <div>
-                <select name="filter" id="filter" class="filterSelect">
-                    <option value="Nome" {{ request('filter') == 'Nome' ? 'selected' : '' }}>Nome</option>
-                    <option value="Descricao" {{ request('filter') == 'Descricao' ? 'selected' : '' }}>Descrição</option>
-                    <option value="Categoria" {{ request('filter') == 'Categoria' ? 'selected' : '' }}>Categoria</option>
-                </select>
-                <input type="search" name="search" class="search" placeholder="Busque um prato (ex: Nome, Descrição ou Categoria)" value="{{ request('search') }}">
-                <button type="button" class="btn btn-secondary clearFilters">Limpar filtros</button>
-                <button class="btn btn-danger btnBuscaDish">Buscar</button>
-            </div>
+                    <div class="filters">
+                        <select name="filter" id="filter" class="filterSelect">
+                            <option value="Nome" {{ request('filter') == 'Nome' ? 'selected' : '' }}>Nome</option>
+                            <option value="Descricao" {{ request('filter') == 'Descricao' ? 'selected' : '' }}>Descrição</option>
+                            <option value="Categoria" {{ request('filter') == 'Categoria' ? 'selected' : '' }}>Categoria</option>
+                        </select>
+                        <input type="search" name="search" class="search" placeholder="Busque um prato (ex: Nome, Descrição ou Categoria)" value="{{ request('search') }}">
+                        <button class="btn btn-danger btnBuscaDish">Buscar</button>
+                        <button type="button" class="btn btn-secondary clearFilters">Limpar filtros</button>
+                    </div>
 
-            @if(empty($paginate['data']))
-                <p>Não há pratos registrados.</p>
-            @else
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th>Imagem</th>
-                            <th>Nome</th>
-                            <th>Descrição</th>
-                            <th>Categoria</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($paginate['data'] as $prato)
-                            <tr>
-                                <td><img src="{{ env('API_URL_STORAGE') }}/{{ $prato['imagem'] }}" alt="{{ $prato['nome'] }}" width="50"></td>
-                                <td>{{ $prato['nome'] }}</td>
-                                <td>{{ $prato['descricao'] }}</td>
-                                <td>{{ ucfirst($prato['categoria']) }}</td>
-                                <td>
-                                    <a href="{{ route('admin.cardapio.edit', ['prato' => $prato['id']]) }}" class="btn-link btn-link-dark">Editar</a>
-                                    <form action="{{ route('admin.cardapio.destroy', ['prato' => $prato['id']]) }}" style="display:inline;" method="POST">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button class="btn btn-danger">Excluir</button>
-                                    </form>
-                                </td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-                <x-pagination :links="$paginate['links']" :currentPage="$paginate['current_page']"
-                    :lastPage="$paginate['last_page']" base-url="{{ route('admin.cardapio.index') }}" />
-                @endif
+                    @if(empty($paginate['data']))
+                        <p>Não há pratos registrados.</p>
+                    @else
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th>Imagem</th>
+                                    <th>Nome</th>
+                                    <th>Descrição</th>
+                                    <th>Categoria</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($paginate['data'] as $prato)
+                                    <tr>
+                                        <td><img src="{{ env('API_URL_STORAGE') }}/{{ $prato['imagem'] }}" alt="{{ $prato['nome'] }}" width="50"></td>
+                                        <td>{{ $prato['nome'] }}</td>
+                                        <td>{{ $prato['descricao'] }}</td>
+                                        <td>{{ ucfirst($prato['categoria']) }}</td>
+                                        <td>
+                                            <a href="{{ route('admin.cardapio.edit', ['prato' => $prato['id']]) }}">Editar</a>
+                                            <form action="{{ route('admin.cardapio.destroy', ['prato' => $prato['id']]) }}" style="display:inline;" method="POST">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit">Excluir</button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                        <x-pagination :links="$paginate['links']" :currentPage="$paginate['current_page']"
+                        :lastPage="$paginate['last_page']" base-url="{{ route('admin.cardapio.index') }}" />
+                    @endif
             </section>
         </div>
     </div>
